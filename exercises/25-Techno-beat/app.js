283c87025cb88487dbf2ae81244b10fd283c0588
gleonag <gleonag@gmail.com>
2022-09-29 17:53:06 +0000

    first commit

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,5 +1,20 @@
 // Add your code here
-
+cont lyricsGenerator = (arr) => {
+var counter = 0;
+const beatArray = arr.map(element)
+return arr.map(element = > {
+    if (element){
+        counter++;
+        if(counter === 3){
+            counter = 0;
+            return "Drop the base !!!Break the base!!!"
+        }
+        return "Drop the base";
+    }
+    counter = 0;
+    return 'Boom';
+})
+return beatArray.join(" ");
 
 // Don't change anything bellow this line
 console.log(lyricsGenerator([0,0,1,1,0,0,0]))
